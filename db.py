e0c947aeeceba71fb65e29ca441c91a49c3af5e1
Shivam <shivamshakya33871@gmail.com>
2022-01-08 21:22:06 +0530

    different square method

---
+++ db.py
@@ -0,0 +1,12 @@
+def square(num)
+    return num*num
+
+l = [1,3,56]
+m = []
+for i in l:
+    m.append(num)
+
+print(m) 
+
+
+
---
+++ pt.py
@@ -0,0 +1,17 @@
+def square(num)
+    return num*num
+
+l = [1,3,56]
+m = []
+for i in l:
+    m.append(square(i))
+print(m)
+
+
+
+print(map(square(2)))
+
+
+
+square = lambda num : num*num
+print(square(6))
